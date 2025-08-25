89ec5dbc7e7dfa3ac80126f5491cd2fa94d4dd25
qiyonghan <qiyonghan10998@xwtec.cn>
2025-08-25 11:10:10 +0800

    修改配置

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.100.22.91:21116,47.100.22.91:21117"];
+pub const RS_PUB_KEY: &str = "ejHEEuKd761NA6Q82WMsnNBaKxYhXhMFs1qdUzrJEPY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
